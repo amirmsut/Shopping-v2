16e544e6e47d3a87492e0f0ee035eacf64fc44a4
Amir <106912785+am1r0d@users.noreply.github.com>
2024-01-04 23:14:54 +0330

    render product list ,

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,9 +1,21 @@
 import { useProducts } from "../context/ProductContext";
 
+import styles from "./ProductPage.module.css";
+
 const ProductPage = () => {
     const products = useProducts();
     console.log(products);
 
-    return <div>ProductPage</div>;
+    return (
+        <div className={styles.container}>
+            <div className={styles.products}>
+                {!products.length && <p>loading...</p>}
+                {products.map((p) => (
+                    <p key={p.id}>{p.title}</p>
+                ))}
+            </div>
+            <div>Side bar</div>
+        </div>
+    );
 };
 export default ProductPage;
