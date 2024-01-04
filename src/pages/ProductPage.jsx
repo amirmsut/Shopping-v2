2edd567faa715d9fc831da4e3e7be54e07e31fb4
Amir <106912785+am1r0d@users.noreply.github.com>
2024-01-05 00:02:30 +0330

    create SearchBox and Category

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,24 +1,70 @@
-import Card from "../components/Card";
+import { useState } from "react";
 import { useProducts } from "../context/ProductContext";
+
+import Card from "../components/Card";
 import Loader from "../components/Loader";
 
 // styles
 import styles from "./ProductPage.module.css";
+import { ImSearch } from "react-icons/im";
+import { FaListUl } from "react-icons/fa";
 
 const ProductPage = () => {
     const products = useProducts();
-    console.log(products);
 
+    const [search, setSearch] = useState("");
+    const searchHandler = () => {
+        console.log("Search");
+    };
+
+    const categoryHandler = (e) => {
+        const { tagName } = e.target;
+        const category = e.target.innerText.toLowerCase();
+        if (tagName !== "LI") return;
+        console.log(category);
+    };
     return (
-        <div className={styles.container}>
-            <div className={styles.products}>
-                {!products.length && <Loader />}
-                {products.map((p) => (
-                    <Card key={p.id} data={p} />
-                ))}
+        <>
+            {/* Search box */}
+            <div>
+                <input
+                    type="text"
+                    placeholder="Search..."
+                    value={search}
+                    onChange={(e) =>
+                        setSearch(e.target.value.toLowerCase().trim())
+                    }
+                />
+                <button onClick={searchHandler}>
+                    <ImSearch />
+                </button>
             </div>
-            <div>Side bar</div>
-        </div>
+
+            {/* Content */}
+            <div className={styles.container}>
+                <div className={styles.products}>
+                    {!products.length && <Loader />}
+                    {products.map((p) => (
+                        <Card key={p.id} data={p} />
+                    ))}
+                </div>
+
+                {/* Sidebar */}
+                <div>
+                    <div>
+                        <FaListUl />
+                        <p>Categories</p>
+                    </div>
+                    <ul onClick={categoryHandler}>
+                        <li>All</li>
+                        <li>Electronics</li>
+                        <li>Jewelery</li>
+                        <li>Men's Clothing</li>
+                        <li>Women's Clothing</li>
+                    </ul>
+                </div>
+            </div>
+        </>
     );
 };
 export default ProductPage;
