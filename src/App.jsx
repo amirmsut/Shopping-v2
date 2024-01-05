6afe254db819590e00ced7fd62c94b0ac4d329ba
Amir <106912785+am1r0d@users.noreply.github.com>
2024-01-05 16:22:51 +0330

    useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,24 @@ import CheckoutPage from "./pages/CheckoutPage";
 import ProductPage from "./pages/ProductPage";
 import PageNotFound from "./pages/404";
 import ProductProvider from "./context/ProductContext";
+import CartProvider from "./context/CartContext";
 
 function App() {
     return (
-        <ProductProvider>
-            <Routes>
-                <Route index element={<Navigate to="/products" replace />} />
-                <Route path="/products" element={<ProductPage />} />
-                <Route path="/products/:id" element={<DetailsPage />} />
-                <Route path="/checkout" element={<CheckoutPage />} />
-                <Route path="/*" element={<PageNotFound />} />
-            </Routes>
-        </ProductProvider>
+        <CartProvider>
+            <ProductProvider>
+                <Routes>
+                    <Route
+                        index
+                        element={<Navigate to="/products" replace />}
+                    />
+                    <Route path="/products" element={<ProductPage />} />
+                    <Route path="/products/:id" element={<DetailsPage />} />
+                    <Route path="/checkout" element={<CheckoutPage />} />
+                    <Route path="/*" element={<PageNotFound />} />
+                </Routes>
+            </ProductProvider>
+        </CartProvider>
     );
 }
 
